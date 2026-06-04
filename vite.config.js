e109f8eaf43a5de53165f6abf6119d5aff1d2cd3
unknown <jeevanandhamsivasankar99@gmail.com>
2026-06-04 11:41:44 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // or @vitejs/plugin-react
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'Jeevacertifications', // Add this exact line with the forward slashes
+  base: 'Jeevacertifications',
 })
